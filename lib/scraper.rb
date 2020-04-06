bfac0b90d5bce1dc0a9d98d3d1e578213b75980e
cruzgerman216 <cruzgerman216@yahoo.com>
2020-04-06 12:29:54 -0700

    text_to_integer method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+
 class Scraper 
     URL = "https://www.worldometers.info/coronavirus/country/us/"
 
@@ -8,17 +9,21 @@ class Scraper
         obj
     end
 
+    def self.text_to_integer(text)
+        text.gsub(/\s+/, "").tap { |s| s.delete!(',') }.to_i
+    end
+    
     def self.scrape_states 
         doc = Nokogiri::HTML(open(URL))
         statesarr = doc.css("tbody tr")
-        
         #51 due to 51 data entries that incldue a non-state "District of Columbia"
         states_length = 51
-        i = 0
+        i = 1
         while i < states_length
             name = statesarr[i].css("td")[0].text.split(" ").join(" ")
-            confirmed_cases = statesarr[i].css("td")[1].text.gsub(/\s+/, "").tap { |s| s.delete!(',') }.to_i
-            overall_deaths =  statesarr[i].css("td")[3].text.gsub(/\s+/, "").tap { |s| s.delete!(',') }.to_i
+
+            confirmed_cases = text_to_integer(statesarr[i].css("td")[1].text)
+            overall_deaths =  text_to_integer(statesarr[i].css("td")[3].text)
            
             if overall_deaths == "" 
                 overall_deaths = 0
